bf17106347053e5a8ee76e202124589e2ecd5958
213 <Olegek5555@mail.ru>
2025-05-12 16:49:00 +0700

    RU admin: add QuizConfig + verbose_name

---
+++ quiz/__init__.py
@@ -0,0 +1,3 @@
+default_app_config = 'quiz.apps.QuizConfig'
+
+
--- quiz/apps.py
+++ quiz/apps.py
@@ -1,5 +1,8 @@
+# quiz/apps.py
 from django.apps import AppConfig
-
+from django.utils.translation import gettext_lazy as _   # «ленивый» перевод
 
 class QuizConfig(AppConfig):
-    name = 'quiz'
+    name = 'quiz'            # путь к пакету
+    verbose_name = _('Викторины')   # так заголовок будет отображаться в админке
+
